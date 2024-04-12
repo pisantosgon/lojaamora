b630d042c091458bfe197be643530112c91aedce
pietra <15pietrasantos@email.com>
2024-04-12 11:13:42 -0300

    Ajustes da Pietra

--- src/Home.js
+++ src/Home.js
@@ -6,37 +6,36 @@ export default function Home()
     return(
         <View style={css.fundo}>
             <Image style={css.imagem}  source={require("../assets/SOBRE.png")}/>
-            <View style={css.texto}>
-                <Text>Em 2009, Bruna Tavares começou seu blog Pausa para Feminices 
+            <View style={css.caixa}>
+                <Text style={css.texto2}>
+                Em 2009, Bruna Tavares começou seu blog Pausa para Feminices 
                 com a intenção de desenvolver apenas um portfólio de jornalismo de moda e beleza,
-
                 mas com o tempo o conteúdo de moda do blog foi sendo substituído por beleza. 
-                 O tempo passou e o Pausa para Feminices se tornou um dos mais respeitadose acessados blogs de beleza de país,
+                O tempo passou e o Pausa para Feminices se tornou um dos mais respeitadose acessados blogs de beleza de país,
                 ficando inclusive em primeiro lugar em uma lista da EXAME de 2015 como dos mais acessados.</Text>
             </View>
         </View>
     )
 }
-const css = StyleSheet.create({
-    
-    texto:{
-        width: "100%",
+const css = StyleSheet.create({    
+    fundo:{       
+        alignItems:'center',           
+    },    
+    caixa:{
         height:400,
-        backgroundColor:"#F1CECC",
-        alignSelf: "center", 
-        marginTop:20,
-        marginLeft:10,
+        width:300,
+        backgroundColor:"#F1CECC",             
       },
     imagem:{
-        width:230,
-        height:230,
-        top:50,
-        resizeMode:"stretch",
-        marginLeft:85
-      },
-      fundo:{
-        backgroundColor:"white"
-      },
-      
-
+        width:"80%",
+        height:180,
+        top:20,    
+      },    
+      texto2:{
+        marginLeft: 20,
+        marginRight: 20,
+        marginTop: 80,
+        marginBottom: 20,
+        textAlign:'justify',
+      }  
 })
